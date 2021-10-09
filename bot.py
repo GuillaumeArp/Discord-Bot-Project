7ca3059ec9771aa2c30ec7999d8cef3297f3c9ce
GuillaumeArp <guillaume.arp@gmail.com>
2021-10-09 16:25:08 +0200

    Script evolution 1

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+
+    print(f'{client.user} is connected to the following guild:\n'
+          f'{guild.name}(id: {guild.id})'
+          )
+
+client.run(TOKEN)
+
+
+
+
+
