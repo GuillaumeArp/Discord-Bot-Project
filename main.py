f874e49c90688a13f2713d7761269f0b15aa7184
GuillaumeArp <guillaume.arp@gmail.com>
2021-10-09 12:02:23 +0200

    Update and secured token.

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import discord
 
 client = discord.Client()
 
-client.run("ODk2MzA3Njc0ODQzNjExMTQ2.YWFNkg.ebv5P12l---U7zaexwgTsPQH71w")
+client.run()
 
 
 @client.event
 async def on_ready():
-    await print("Odysseus est prêt !")
+    print("Odysseus est prêt !")
+
+@client.event
+async def on_disconnect():
+    print("Bye !")
 
 
 @client.event
 async def on_message(message):
     if message.content == "Ping":
         await message.channel.send("Pong")
-
